d979e958efdb7fea83cc1bf53e03fde337ed0414
shondoe11 <shawnhadritan@gmail.com>
2025-03-07 18:26:08 +0800

    update user model add display_name

--- server/model.py
+++ server/model.py
@@ -9,6 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     spotify_id = db.Column(db.String(128), unique=True, nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
+    display_name = db.Column(db.String(128))
     role = db.Column(db.String(20), default='guest')  #~ values: guest, regular, promoter
     oauth_token = db.Column(db.String(512))
     refresh_token = db.Column(db.String(512))
