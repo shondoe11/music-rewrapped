5dd2a3cacad5ce931e59734e70be2f168d4bd3a1
shondoe11 <shawnhadritan@gmail.com>
2025-03-08 21:50:26 +0800

    init routes in App and global wrapper for ToastContainer in main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Navigate to='/login' />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </Router>
   )
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,29 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './globals.css'
+import { AuthProvider } from './context/AuthProvider.jsx'
+import { Bounce, Flip, Slide, ToastContainer, Zoom } from 'react-toastify'
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+      <ToastContainer
+        position="top-center"
+        autoClose={1500}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Flip}
+        />
+    </AuthProvider>
+  </React.StrictMode>,
 )
