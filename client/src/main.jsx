35919103153a1d5a9c5de6da054d5fc2a3413681
shondoe11 <shawnhadritan@gmail.com>
2025-03-10 12:01:15 +0800

    update toastify import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './globals.css'
+//* use spotify design guidelines for fonts/colors as per https://developer.spotify.com/documentation/design
 import { AuthProvider } from './context/AuthProvider.jsx'
+import 'react-toastify/dist/ReactToastify.css';
 import { Bounce, Flip, Slide, ToastContainer, Zoom } from 'react-toastify'
-
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
