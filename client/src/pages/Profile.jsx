fc5f53fc4a8c02608e0a2fa5ccfd415759228538
shondoe11 <shawnhadritan@gmail.com>
2025-03-11 18:11:36 +0800

    init Profile pg

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl">Your Profile</h1>
+    </div>
+  );
+};
+
+export default Profile;
