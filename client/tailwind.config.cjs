5199b9aba53027803883cf1e410ef8cbbf68400b
shondoe11 <shawnhadritan@gmail.com>
2025-03-17 21:29:03 +0800

    updated tw config to include reactbits styles

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -5,7 +5,28 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                'star-movement-bottom': 'star-movement-bottom linear infinite alternate',
+                'star-movement-top': 'star-movement-top linear infinite alternate',
+                'gradient': 'gradient 8s linear infinite'
+            },
+            keyframes: {
+                'star-movement-bottom': {
+                    '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+                    '100%': { transform: 'translate(-100%, 0%)', opacity: '0' },
+                },
+                'star-movement-top': {
+                    '0%': { transform: 'translate(0%, 0%)', opacity: '1' },
+                    '100%': { transform: 'translate(100%, 0%)', opacity: '0' },
+                },
+                'gradient': {
+                    '0%': { backgroundPosition: '0% 50%' },
+                    '50%': { backgroundPosition: '100% 50%' },
+                    '100%': { backgroundPosition: '0% 50%' },
+                },
+            },
+        },
     },
     plugins: [],
 };
