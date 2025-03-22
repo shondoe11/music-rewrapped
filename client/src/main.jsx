271f8c216838e1b8a6b4c4340f4abdecbae58fc7
shondoe11 <shawnhadritan@gmail.com>
2025-03-22 12:22:40 +0800

    init SplashCursor style

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import './globals.css'
 //* use spotify design guidelines for fonts/colors as per https://developer.spotify.com/documentation/design
 import { AuthProvider } from './context/AuthProvider.jsx'
 import 'react-toastify/dist/ReactToastify.css';
 import { Bounce, Flip, Slide, ToastContainer, Zoom } from 'react-toastify'
+import SplashCursor from './styles/animations/SplashCursor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SplashCursor 
+      DENSITY_DISSIPATION={5}
+      COLOR_UPDATE_SPEED={15}
+      SPLAT_RADIUS={0.1}
+      SPLAT_FORCE={9000}
+      TRANSPARENT={true}
+    />
     <AuthProvider>
       <App />
       <ToastContainer
