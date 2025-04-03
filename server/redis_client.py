7b45bedbbdfa653068d48fb4ec8becaccb33ab78
shondoe11 <shawnhadritan@gmail.com>
2025-04-03 15:01:53 +0800

    update env config condition

--- server/redis_client.py
+++ server/redis_client.py
@@ -1,16 +1,21 @@
 import os
 import redis
 from dotenv import load_dotenv
-load_dotenv()
 
-#* read connection info from env (set defaults to local first - testing)
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
-REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
-REDIS_DB = os.environ.get('redis_db', 0)
+#& determine env based on 'FLASK_ENV'; default is development
+env = os.environ.get('FLASK_ENV', 'development')
+load_dotenv(dotenv_path=f".env.{env}")  #~ load appropriate .env file
 
-redis_client = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    db=REDIS_DB,
-    decode_responses=True #~ auto decode strings to UTF-8
-)
+#& if redis url provided (prod), use it; else fallback dev settings
+if os.environ.get('REDIS_URL'):
+    redis_client = redis.Redis.from_url(os.environ.get('REDIS_URL'), decode_responses=True)
+else:
+    REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+    REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
+    REDIS_DB = os.environ.get('REDIS_DB', 0)
+    redis_client = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        db=REDIS_DB,
+        decode_responses=True  #~ auto decode strings to UTF-8
+    )
