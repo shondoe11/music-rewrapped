b5c892f3f5abe4c90e04b6a5f32a864085f60d74
shondoe11 <shawnhadritan@gmail.com>
2025-04-04 15:19:35 +0800

    update fr prod

--- server/config.py
+++ server/config.py
@@ -35,6 +35,8 @@ class DevelopmentConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_DOMAIN = os.environ.get("SESSION_COOKIE_DOMAIN", "my-production-domain.com")
 
 class TestingConfig(Config):
     TESTING = True
